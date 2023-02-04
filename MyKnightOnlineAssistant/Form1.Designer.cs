ae4e26dd4e23466648518159e92de4188f8814b5
MesutRuzgar <mesutruzgar@gmail.com>
2023-02-04 22:32:13 +0300

    Pus calculator added

--- MyKnightOnlineAssistant/Form1.Designer.cs
+++ MyKnightOnlineAssistant/Form1.Designer.cs
@@ -28,13 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gbxPus = new System.Windows.Forms.GroupBox();
+            this.lbl400npoint = new System.Windows.Forms.Label();
+            this.tbx400npoint = new System.Windows.Forms.TextBox();
+            this.tbxGb = new System.Windows.Forms.TextBox();
+            this.lblGb = new System.Windows.Forms.Label();
+            this.tbxPus = new System.Windows.Forms.TextBox();
+            this.lblCalcPus = new System.Windows.Forms.Label();
+            this.tbxCoins = new System.Windows.Forms.TextBox();
+            this.lblCoins = new System.Windows.Forms.Label();
+            this.btnPus = new System.Windows.Forms.Button();
+            this.gbxPus.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbxPus
+            // 
+            this.gbxPus.Controls.Add(this.btnPus);
+            this.gbxPus.Controls.Add(this.tbxCoins);
+            this.gbxPus.Controls.Add(this.lblCoins);
+            this.gbxPus.Controls.Add(this.tbxPus);
+            this.gbxPus.Controls.Add(this.lblCalcPus);
+            this.gbxPus.Controls.Add(this.tbxGb);
+            this.gbxPus.Controls.Add(this.lblGb);
+            this.gbxPus.Controls.Add(this.tbx400npoint);
+            this.gbxPus.Controls.Add(this.lbl400npoint);
+            this.gbxPus.Font = new System.Drawing.Font("Corbel", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.gbxPus.Location = new System.Drawing.Point(12, 48);
+            this.gbxPus.Name = "gbxPus";
+            this.gbxPus.Size = new System.Drawing.Size(318, 226);
+            this.gbxPus.TabIndex = 0;
+            this.gbxPus.TabStop = false;
+            this.gbxPus.Text = "PUS HESAPLAYICI";
+            // 
+            // lbl400npoint
+            // 
+            this.lbl400npoint.AutoSize = true;
+            this.lbl400npoint.Location = new System.Drawing.Point(14, 44);
+            this.lbl400npoint.Name = "lbl400npoint";
+            this.lbl400npoint.Size = new System.Drawing.Size(178, 18);
+            this.lbl400npoint.TabIndex = 0;
+            this.lbl400npoint.Text = "400 NPOİNT TL KARŞILIĞI:";
+            // 
+            // tbx400npoint
+            // 
+            this.tbx400npoint.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.tbx400npoint.Location = new System.Drawing.Point(202, 41);
+            this.tbx400npoint.Name = "tbx400npoint";
+            this.tbx400npoint.Size = new System.Drawing.Size(100, 29);
+            this.tbx400npoint.TabIndex = 1;
+            // 
+            // tbxGb
+            // 
+            this.tbxGb.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.tbxGb.Location = new System.Drawing.Point(202, 76);
+            this.tbxGb.Name = "tbxGb";
+            this.tbxGb.Size = new System.Drawing.Size(100, 29);
+            this.tbxGb.TabIndex = 2;
+            // 
+            // lblGb
+            // 
+            this.lblGb.AutoSize = true;
+            this.lblGb.Location = new System.Drawing.Point(14, 79);
+            this.lblGb.Name = "lblGb";
+            this.lblGb.Size = new System.Drawing.Size(158, 18);
+            this.lblGb.TabIndex = 2;
+            this.lblGb.Text = "GOLD BAR ALIŞ FİYATI:";
+            // 
+            // tbxPus
+            // 
+            this.tbxPus.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.tbxPus.Location = new System.Drawing.Point(202, 111);
+            this.tbxPus.Name = "tbxPus";
+            this.tbxPus.Size = new System.Drawing.Size(100, 29);
+            this.tbxPus.TabIndex = 3;
+            // 
+            // lblCalcPus
+            // 
+            this.lblCalcPus.AutoSize = true;
+            this.lblCalcPus.Location = new System.Drawing.Point(14, 114);
+            this.lblCalcPus.Name = "lblCalcPus";
+            this.lblCalcPus.Size = new System.Drawing.Size(177, 18);
+            this.lblCalcPus.TabIndex = 4;
+            this.lblCalcPus.Text = "HESAPLANACAK NPOİNT:";
+            // 
+            // tbxCoins
+            // 
+            this.tbxCoins.Enabled = false;
+            this.tbxCoins.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.tbxCoins.Location = new System.Drawing.Point(202, 150);
+            this.tbxCoins.Name = "tbxCoins";
+            this.tbxCoins.Size = new System.Drawing.Size(100, 29);
+            this.tbxCoins.TabIndex = 333;
+            // 
+            // lblCoins
+            // 
+            this.lblCoins.AutoSize = true;
+            this.lblCoins.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCoins.Location = new System.Drawing.Point(14, 155);
+            this.lblCoins.Name = "lblCoins";
+            this.lblCoins.Size = new System.Drawing.Size(182, 15);
+            this.lblCoins.TabIndex = 6;
+            this.lblCoins.Text = "OYUNDAKİ COİNS KARŞILIĞI:";
+            // 
+            // btnPus
+            // 
+            this.btnPus.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btnPus.Location = new System.Drawing.Point(202, 182);
+            this.btnPus.Name = "btnPus";
+            this.btnPus.Size = new System.Drawing.Size(100, 33);
+            this.btnPus.TabIndex = 334;
+            this.btnPus.Text = "Hesapla";
+            this.btnPus.UseVisualStyleBackColor = true;
+            this.btnPus.Click += new System.EventHandler(this.btnPus_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(650, 503);
+            this.Controls.Add(this.gbxPus);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.gbxPus.ResumeLayout(false);
+            this.gbxPus.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gbxPus;
+        private System.Windows.Forms.TextBox tbxCoins;
+        private System.Windows.Forms.Label lblCoins;
+        private System.Windows.Forms.TextBox tbxPus;
+        private System.Windows.Forms.Label lblCalcPus;
+        private System.Windows.Forms.TextBox tbxGb;
+        private System.Windows.Forms.Label lblGb;
+        private System.Windows.Forms.TextBox tbx400npoint;
+        private System.Windows.Forms.Label lbl400npoint;
+        private System.Windows.Forms.Button btnPus;
     }
 }
 
--- MyKnightOnlineAssistant/Form1.cs
+++ MyKnightOnlineAssistant/Form1.cs
@@ -16,5 +16,45 @@ namespace MyKnightOnlineAssistant
         {
             InitializeComponent();
         }
+
+        private void btnPus_Click(object sender, EventArgs e)
+        {
+            if (tbx400npoint.Text == "" && tbxGb.Text == "" && tbxPus.Text == "")
+            {
+
+                MessageBox.Show("Lütfen Gerekli Alanları Doldurunuz.");
+                tbx400npoint.Focus();
+            }
+            else if (tbx400npoint.Text == "")
+            {
+                MessageBox.Show("Lütfen 400 NPOİNT'in TL Karşılığını Giriniz.");
+                tbx400npoint.Focus();
+            }
+            else if (tbxGb.Text == "")
+            {
+                MessageBox.Show("Lütfen  Gold Bar Alış Fiyatını Giriniz.");
+                tbxGb.Focus();
+            }
+            else if (tbxPus.Text == "")
+            {
+
+                MessageBox.Show("Lütfen Hesaplanacak NPOİNT Miktarını Giriniz.");
+                tbxPus.Focus();
+            }
+            else
+            {
+
+                double pus, sonuc,coinsTutar;
+                int gbTl, calcPus;
+                calcPus = Convert.ToInt32(tbxPus.Text);
+                pus = Convert.ToDouble(tbx400npoint.Text);
+                gbTl = Convert.ToInt32(tbxGb.Text);
+                sonuc = pus / 400 * calcPus / gbTl * 100000;
+                coinsTutar = sonuc;
+                tbxCoins.Text = coinsTutar.ToString("N3").Replace(".", ",");
+
+            }
+
+        }
     }
 }
